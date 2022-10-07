bfa6102a73249efc3b0a3c00898bebea2cd6e24a
Vu Anh <anhv.ict91@gmail.com>
2022-10-07 12:53:18 +0700

    update

--- exercises/sublist/src/lib.rs
+++ exercises/sublist/src/lib.rs
@@ -1,5 +1,5 @@
 use std::hash::Hash;
-use itertools::Itertools;
+use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Comparison {
@@ -9,28 +9,37 @@ pub enum Comparison {
     Unequal,
 }
 
+fn count_freqs<T:Eq+Hash>(x: &[T]) -> HashMap<&T, usize> {
+  x.iter().fold(HashMap::new(), |mut m, val| {
+      m.entry(val).and_modify(|freq| { *freq += 1 }).or_insert(1);
+      m
+  })
+}
+
 pub fn sublist<T: Hash + Eq + PartialEq>(_v1: &[T], _v2: &[T]) -> Comparison {
   if _v1.len() > _v2.len() {
-    return sublist(_v2, _v1);
+    let result = sublist(_v2, _v1);
+    if result == Comparison::Sublist {
+      return Comparison::Superlist;
+    } else {
+      return result;
+    }
   }
-  let m1 = _v1.into_iter().counts();
-  let m2 = _v2.into_iter().counts();
+  let m1 = count_freqs(&_v1);
+  let m2 = count_freqs(&_v2);
   for (k, f1) in m1.iter() {
       match m2.get(k) {
           Some(f2) => if f1 > f2 {
-              return -1;
-          } else {
-              return 0;
+              return Comparison::Unequal;
           }
           None => {
-              return -2;
+              return Comparison::Unequal;
           }
       }
   }
-
-  // let count = [1, 1, 1, 3, 3, 5].into_iter().counts();
-  // let d1 = s1.difference(&s2).count();
-  // let d2 = s2.difference(&s1).count();
-  println!("{:?}", m1);
-  return Comparison::Unequal;
+  if _v1.len() == _v2.len() {
+    return Comparison::Equal;
+  } else {
+    return Comparison::Sublist;
+  }
 }
--- exercises/sublist/tests/lib.rs
+++ exercises/sublist/tests/lib.rs
@@ -6,3 +6,31 @@ fn sublist_1() {
   let v2 = &[1, 2, 3];
   assert_eq!(sublist(v1, v2), Comparison::Equal);
 }
+
+#[test]
+fn sublist_2() {
+  let v1 = &[1, 2, 3, 1];
+  let v2 = &[1, 2, 3];
+  assert_eq!(sublist(v1, v2), Comparison::Superlist);
+}
+
+#[test]
+fn sublist_3() {
+  let v1 = &[1, 2, 3];
+  let v2 = &[1, 2, 3, 4];
+  assert_eq!(sublist(v1, v2), Comparison::Sublist);
+}
+
+#[test]
+fn sublist_4() {
+  let v1 = &[1, 2, 3, 2];
+  let v2 = &[1, 2, 3, 4];
+  assert_eq!(sublist(v1, v2), Comparison::Unequal);
+}
+
+#[test]
+fn sublist_5() {
+  let v1 = &[1, 3];
+  let v2 = &[1, 2, 3];
+  assert_eq!(sublist(v1, v2), Comparison::Unequal);
+}
