1cd218397cf0242ca0ed395c2696ea1a8a6d7620
Vu Anh <anhv.ict91@gmail.com>
2022-10-12 07:21:29 +0700

    update

--- exercises/poker/tests/lib.rs
+++ exercises/poker/tests/lib.rs
@@ -12,3 +12,8 @@ fn hs_from<'a>(input: &[&'a str]) -> HashSet<&'a str> {
 fn test<'a, 'b>(input: &[&'a str], expected: &[&'b str]) {
     assert_eq!(hs_from(&winning_hands(input)), hs_from(expected))
 }
+
+#[test]
+fn test_single_hand_always_wins() {
+    test(&["4S 5S 7H 8D JC"], &["4S 5S 7H 8D JC"])
+}
